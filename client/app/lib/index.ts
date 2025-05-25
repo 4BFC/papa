c6989373ed915e8eba267977cfaee5ef7ff76806
4BFC <4bee.code@gamil.com>
2025-05-25 22:03:33 +0900

    ✨: axios, react-qeury 설치 및 eslint, tsconfig 설정
    - 컴파일 과정에서 오류 체크가 나아지 않아 이를 lint 규칙과 tsconfig로 규칙 강화

--- client/app/lib/index.ts
+++ client/app/lib/index.ts
@@ -1,2 +1,5 @@
 export * from "./axios/axiosInstance";
 export * from "./axios/axiosinterceptors";
+export * from "./axios/errorHandlers";
+export * from "./axios/transformers";
+export * from "./axios/types";
--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,3 +1,5 @@
+import pluginTs from "@typescript-eslint/eslint-plugin";
+import parserTs from "@typescript-eslint/parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -10,6 +12,29 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: parserTs,
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": pluginTs,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-unsafe-call": "error",
+      "@typescript-eslint/no-unsafe-member-access": "error",
+    },
+  },
+
+  // next/core-web-vitals 및 typescript 호환을 위해 compat.extends 사용
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
