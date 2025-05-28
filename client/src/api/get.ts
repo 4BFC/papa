1cff6c8c3e2159b0bcfcbbba18b2bad80c9eb0c3
4BFC <4bee.code@gamil.com>
2025-05-28 10:59:09 +0900

    ✨: 각 메소드 함수들의 error의 타입 가드 설정
    - error가 정해진 타입으로 구현되지 않을 수 있기 때문에 타입 가드를 설정해서 타입 검증 후 유연하게 대응할 수 있게 코드 구현

--- client/src/api/get.ts
+++ client/src/api/get.ts
@@ -5,7 +5,12 @@ const get = async <T>(url: string): Promise<T> => {
     const response = await axiosInstance.get<T>(url);
     return response.data;
   } catch (error: unknown) {
-    console.error(error);
+    /**타입 가드 설정 */
+    if (error instanceof Error) {
+      console.error(error);
+    } else {
+      console.error("예외 타입 Error", String(error));
+    }
   }
 };
 
