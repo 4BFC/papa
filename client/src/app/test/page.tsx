7493ac9e30cd63769034351201cf4d876bb7ec3b
4BFC <4bee.code@gamil.com>
2025-05-28 17:30:07 +0900

    ✨: Nextjs server-side를 위해 api 디렉토리에서 사용되는 axios들을 client로 이동
    - CORS문제를 해결하기 위해서 서버가 아닌 server-side로 문제를 해결할 예정

--- client/src/app/test/page.tsx
+++ client/src/app/test/page.tsx
@@ -3,15 +3,11 @@
 import { useFetch } from "@/hook";
 import { get } from "@/api";
 import { LedgerModel } from "@/types";
-// import "@/lib/axios/axiosInterceptors";
+import "@/lib/axios/axiosInterceptors";
 
 const Test = (): React.ReactNode => {
-  // const URL: string = process.env.MY_API || "";
-  const { isData } = useFetch<LedgerModel[]>(
-    () =>
-      get("https://bqqtkzywfttjzwswydxp.supabase.co/functions/v1/api/ledger"),
-    true
-  );
+  const URL: string = process.env.NEXT_PUBLIC_MY_API || "";
+  const { isData } = useFetch<LedgerModel[]>(() => get(`${URL}/ledger`), true);
 
   console.log(isData);
 
