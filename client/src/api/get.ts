c0ff36a182de0cb7b7280644592ff19b8a44cc0d
4BFC <4bee.code@gamil.com>
2025-05-28 13:34:44 +0900

    ✨: get 함수에서 예외처리 방식 보완

--- client/src/api/get.ts
+++ client/src/api/get.ts
@@ -8,8 +8,10 @@ const get = async <T>(url: string): Promise<T> => {
     /**타입 가드 설정 */
     if (error instanceof Error) {
       console.error(error);
+      throw error;
     } else {
       console.error("예외 타입 Error", String(error));
+      throw new Error("알 수 없는 에러가 발생했습니다.");
     }
   }
 };
