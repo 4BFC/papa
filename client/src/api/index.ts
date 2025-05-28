95d401e337d4ff6f52e349246905aaf97246e644
4BFC <4bee.code@gamil.com>
2025-05-28 20:36:45 +0900

    ✨: server-side를 위한 axios Instance 생성
    - server-side로 구현을 하면 환경 변수 노출이 client 비해 안전하다.

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,3 +1,5 @@
 export { default as post } from "./client/post";
 export { default as put } from "./client/put";
 export { default as get } from "./client/get";
+
+export { default as axiosServer } from "./server/axiosServer";
--- client/src/api/server/axiosServer.ts
+++ client/src/api/server/axiosServer.ts
@@ -1,5 +1,12 @@
-// import axios, { axiosInstance } from "axios";
+import axios, { AxiosInstance } from "axios";
 
-// const axiosServer = axios.create({
-//   baseURL: "http://localhost:3000",
-// })
+const axiosServer: AxiosInstance = axios.create({
+  baseURL: `${process.env.MY_API}`,
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.MY_ANON_KEY}`,
+  },
+});
+
+export default axiosServer;
