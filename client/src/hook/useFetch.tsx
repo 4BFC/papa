f852f06e2fd05e90cf53bf4c00aecae50667399e
4BFC <4bee.code@gamil.com>
2025-05-28 13:33:46 +0900

    ✨: useFetch에서 매개변수를 매개함수로 변경 및 타입 조정
    - useEffect 의존성 배열에 함수로 인해 발생한 문제 해결, 현재는 제거만 해두었지만 추후에 get 함수 또는 get을 불러올 때 useCallback 사용 필요.

--- client/src/hook/useFetch.tsx
+++ client/src/hook/useFetch.tsx
@@ -1,19 +1,29 @@
+"use client";
+
 import { useEffect, useState } from "react";
 
-const useFetch = (type: Promise<T>, enabled: boolean): { isData: T } => {
+const useFetch = (
+  type: () => Promise<T>,
+  enabled: boolean
+): { isData: T | null } => {
   const [isData, setData] = useState<T>();
   useEffect(() => {
     if (!enabled) return;
     const fetchData = async (): void => {
       try {
-        const response = await type;
+        const response = await type();
         setData(response);
-      } catch (error) {
-        console.error(error);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error(error);
+        } else {
+          console.error("예외 타입 Error", String(error));
+        }
       }
     };
     fetchData();
-  }, [enabled, type]);
+    /**type을 의존성 배열에 넣으면 무한 렌더링 발생 따라서 외부에서 useCallback으로 감싸서 의존성 배열에 넣어주는게 좋다. */
+  }, [enabled]);
   return { isData };
 };
 export default useFetch;
