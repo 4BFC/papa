7306a0e3a9121bd8be5bdb82f52452aa797da3f0
4BFC <4bee.code@gamil.com>
2025-05-28 20:38:16 +0900

    ✨: test page에서 fetch로 api 연동 확인
    - hook으로 코드를 커스텀할 예정이다.

--- client/src/app/test/page.tsx
+++ client/src/app/test/page.tsx
@@ -1,16 +1,31 @@
 "use client";
 
-import { useFetch } from "@/hook";
-import { get } from "@/api";
-import { LedgerModel } from "@/types";
+// import { useFetch } from "@/hook";
+// import { get } from "@/api";
+// import { LedgerModel } from "@/types";
 import "@/lib/axios/axiosInterceptors";
+import { useEffect } from "react";
 
 const Test = (): React.ReactNode => {
-  const URL: string = process.env.NEXT_PUBLIC_MY_API || "";
-  const { isData } = useFetch<LedgerModel[]>(() => get(`${URL}/ledger`), true);
+  // const URL: string = process.env.NEXT_PUBLIC_MY_API || "";
+  // const { isData } = useFetch<LedgerModel[]>(() => get(`${URL}/ledger`), true);
 
-  console.log(isData);
+  // console.log(isData);
 
+  const fetchLedger = async (): Promise<void> => {
+    try {
+      const res = await fetch("/api/ledger"); // ✅ Next API Route 호출
+      if (!res.ok) throw new Error("서버 응답 실패");
+
+      const data = await res.json();
+      console.log("Ledger:", data);
+    } catch (error) {
+      console.error("Ledger 불러오기 실패:", error);
+    }
+  };
+  useEffect(() => {
+    fetchLedger();
+  }, []);
   return <div>Test</div>;
 };
 
