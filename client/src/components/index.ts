8a61ceea8feb3fa7a70ed9955b0db789091f845d
4BFC <4bee.code@gamil.com>
2025-06-04 23:37:46 +0900

    ✨: QA 부분을 적용한 디자인 변경
    - `이득금액이 나오는 5줄을 구현 했으면 좋겠다.`는 클라이언트 의뢰: 텍스트의 크기를 줄이고 padding의 높이와 여분을 만들어서 시각적으로 편안하게 구현
    - `모든 표의 선이 표현되어서 어지럽다.`는 클라이언트의 의뢰: bottom 부분만 선이 나타 날 수 있게 border-b를 적용

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,2 +1,4 @@
+export { default as LegacyHeaderRow } from "./ui/Legacy_HeaderRow";
+export { default as LegacyDataRow } from "./ui/Legacy_DataRow";
 export { default as HeaderRow } from "./ui/HeaderRow";
 export { default as DataRow } from "./ui/DataRow";
--- client/src/components/ui/DataRow.tsx
+++ client/src/components/ui/DataRow.tsx
@@ -1,48 +1,24 @@
-import { useState } from "react";
 import { LedgerModel } from "@/types";
 
 const DataRow = ({ data }: { data: LedgerModel }): React.ReactElement => {
-  const [isButtonActive, setButtonActive] = useState<boolean>(true);
-
   return (
-    <>
-      <div
-        className="grid grid-cols-4 bg-gray-100"
-        onClick={() => setButtonActive((prev) => !prev)}
-      >
-        <div className="border border-gray-300 p-2">{data.item}</div>
-        <div className="border border-gray-300 p-2">{data.count}</div>
-        <div className="border border-gray-300 p-2">{data.salePrice}</div>
-        <div className="border border-gray-300 p-2">{data.costPrice}</div>
-      </div>
-      <div
-        className="grid grid-cols-2 bg-gray-100"
-        onClick={() => setButtonActive((prev) => !prev)}
-      >
-        <div className="border border-gray-300 p-1">이득 금액</div>
-        <div className="border border-gray-300 p-1">{data.profit}</div>
-      </div>
-      {/* 버튼 행 */}
-      <div className="grid grid-cols-4">
-        <div
-          className={`w-full border border-gray-300 col-span-4 flex flex-col justify-center items-center transition-all duration-300 ease-in-out ${
-            isButtonActive
-              ? "max-h-0 opacity-0 transform scale-y-0 origin-top p-0"
-              : "max-h-[100px] opacity-100 transform scale-y-100 origin-top p-2"
-          }`}
-        >
-          <div className="flex w-6/12 gap-4">
-            <button className="w-1/2 bg-blue-500 text-white p-2 rounded-md">
-              수정
-            </button>
-            <button className="w-1/2 bg-red-400 text-white p-2 rounded-md">
-              삭제
-            </button>
-          </div>
-          <div>작성일 : 2025.05.29</div>
+    <div className="overflow-x-auto">
+      <div className="w-full flex justify-center items-center text-sm font-sans border-b border-gray-200">
+        <div className="w-20/100 bg-white py-3 px-2 text-left">{data.item}</div>
+        <div className="w-15/100 bg-white py-3 px-2 text-center">
+          {data.count}
+        </div>
+        <div className="w-25/100 bg-white py-3 px-2 text-right">
+          {data.salePrice}
+        </div>
+        <div className="w-20/100 bg-white py-3 px-2 text-right">
+          {data.costPrice}
+        </div>
+        <div className="w-20/100 bg-white py-3 px-2 text-right text-green-600 font-medium">
+          {data.profit}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
