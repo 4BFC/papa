9b42b6cdc0aaea12d70ce00dbdc14741d7a8c138
4BFC <4bee.code@gmail.com>
2025-07-20 18:39:25 +0900

    🧪: 전역상태를 테스트를 하기 위한 test 컴포넌트 생성 및 코드 수정

---
+++ client/src/widgets/secretary/input/presentational/InputForm.test.tsx
@@ -0,0 +1,34 @@
+import InputFormItemTest from "@/features/secretary/input/ui/InputFormItem.test";
+// import useFetchAPI from "@/features/secretary/input/model/useFetchAPI";
+import useInputFormTest from "@/features/secretary/input/model/useInputForm.test";
+// import {
+//   LedgerRequire,
+//   PaymentRequire,
+//   LedgerDataResponse,
+//   PaymentDataResponse,
+// } from "@/shared/types";
+// import { Dispatch, SetStateAction } from "react";
+
+const InputForm = (): React.ReactElement => {
+  const { setTax, register, handleSubmit, errors, onSubmit } =
+    useInputFormTest();
+
+  return (
+    <InputFormItemTest
+      // isHeaderActive={isHeaderActive}
+      // setHeaderActive={setHeaderActive}
+      // setComplexPayment={setComplexPayment}
+      //   handleActive={handleActive}
+      // getLoading={getLoading}
+      // postLoading={postLoading}
+      handleSubmit={handleSubmit}
+      onSubmit={onSubmit}
+      register={register}
+      errors={errors}
+      setTax={setTax}
+      // isComplexPayment={isComplexPayment}
+    />
+  );
+};
+
+export default InputForm;
