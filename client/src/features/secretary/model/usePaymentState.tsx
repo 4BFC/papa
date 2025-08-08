5d40e73a88edf39bfbd5d270adbb74cc2b382caf
4BFC <4bee.code@gmail.com>
2025-08-09 02:51:40 +0900

    🧪 : input과 header의 checkbox와 toggle에 따라 주입되어 상태를 반환하는 함수 구현
    - 현재는 코드가 주석으로 되어 있지만, 추후에 코드를 개선 시켜서 UX 동작일 명확하게 할 수 있게 구현할 예정

---
+++ client/src/features/secretary/model/usePaymentState.tsx
@@ -0,0 +1,20 @@
+// // import useSecretaryContext from "@/views/secretary/context/useSecretaryContext";
+// import { PaymentType } from "@/shared/types/secretary/paymentType";
+// // 결제 상태에 따라 복합 결제와 카드 결제를 반환하는 코드
+// const usePaymentState = (type: PaymentType) => {
+//   // const { isTax, isComplexPayment } = useSecretaryContext();
+//   card: {
+//     isTax: true,
+//     isComplexPayment: false,
+//   },
+//   complex: {
+//     isTax: true,
+//     isComplexPayment: true,
+//   },
+//   default: {
+//     isTax: false,
+//     isComplexPayment: false,
+//   },
+// };
+
+// export default usePaymentState;
