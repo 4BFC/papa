12d75cc608054941156494ff608c9bf1ecff6526
4BFC <4bee.code@gmail.com>
2025-08-09 18:18:18 +0900

    ✨: usePaymentValidation을 통해서 isTax, isComplexPayment에 따른 상태를 순수함수로 관리한다.

---
+++ client/src/features/secretary/model/usePaymentValidation.ts
@@ -0,0 +1,25 @@
+import {
+  PaymentType,
+  PaymentFlags,
+} from "@/shared/types/secretary/paymentType";
+
+const PAYMENT_STATES: Record<PaymentType, PaymentFlags> = {
+  card: { isTax: true, isComplexPayment: false },
+  complex: { isTax: true, isComplexPayment: true },
+  default: { isTax: false, isComplexPayment: false },
+};
+
+// 결제 상태에 따라 복합 결제와 카드 결제를 반환하는 코드
+export const setPaymentValidation = (type: PaymentType): PaymentFlags => {
+  return PAYMENT_STATES[type];
+};
+export const getPaymentValidation = (
+  isTax: boolean,
+  isComplexPayment: boolean
+): PaymentFlags => {
+  if (!isTax && !isComplexPayment) return "default";
+  if (isTax && !isComplexPayment) return "card";
+  if (isTax && isComplexPayment) return "complex";
+  return "default";
+};
+// export default { getPaymentValidation, setPaymentValidation };
